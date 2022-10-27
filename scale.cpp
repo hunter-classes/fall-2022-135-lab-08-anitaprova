174ee62258383497fcad242460f6d4639048c224
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-27 19:22:38 -0400

    finished

--- scale.cpp
+++ scale.cpp
@@ -6,24 +6,15 @@
 void scale (std::string filename, int image[MAX_H][MAX_W], int height, int width ) {
 	readImage(filename, image, height, width);
 
-	int c[MAX_H][MAX_W];
-	for (int row = 0; row < height; row++) { //copy whole thing
-		for (int col = 0; col < width; col++) {
-			c[row][col] = image[row][col];
-		}
-	}
-	
 	int out[MAX_H][MAX_W];
-	for (int row = 0; row < height * 2; row++) {
-		for (int col = 0; col < width * 2; col++) {
-			out[row][col] = c[row][col];
-			out[row][col+1] = c[row][col];
-			out[row+1][col] = c[row][col];
-			out[row+1][col+1] = c[row][col];
-			col++;
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			out[row * 2][col * 2] = image[row][col];
+			out[row * 2][col * 2 + 1] = image[row][col];
+			out[row * 2 + 1][col * 2] = image[row][col];
+			out[row * 2 + 1][col * 2 + 1] = image[row][col];
 		}
-		row++;
 	}
 	
-	writeImage("taskE.pgm", out, height, width);
+	writeImage("taskE.pgm", out, height*2, width*2);
 }
